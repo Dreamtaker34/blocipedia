ca0da8c87bb8f0b0e0c18044fe227cc207bdd210
Dreamtaker34 <dreamtaker@gmail.com>
2016-01-05 15:40:18 -0500

    Blocipedia - User Roles complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ActiveRecord::Base
+  after_initialize :init
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  enum role: [:standard, :premium, :admin]
+
+  def init
+    self.role   ||= :standard    
+  end
 end
