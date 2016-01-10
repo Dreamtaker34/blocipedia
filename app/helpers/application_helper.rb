b731f210a2e2b2a2e6b3ffe122e19ecba104a59c
Dreamtaker34 <dreamtaker@gmail.com>
2016-01-10 00:17:42 -0500

    Blocipedia - Markdown complete

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  def markdown(text)
+    render_options = {
+      filter_html: true,
+      hard_wrap: true,
+    }
+    renderer = Redcarpet::Render::HTML.new(render_options)
+
+    extensions = {
+      autolink: true,
+      fenced_code_blocks: true,
+      lax_spacing: true,
+      no_intra_emphasis: true,
+      strikethrough: true,
+      superscript: true,
+    }
+    Redcarpet::Markdown.new(renderer, extensions).render(text).html_safe
+  end
 end
